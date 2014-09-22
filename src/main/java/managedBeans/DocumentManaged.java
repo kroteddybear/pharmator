aded350778697ae54aa3be57a48fd7fbbb4723ee
Teddy <Teddy@172.16.126.125>
2014-09-22 15:03:41 +0200

    add beans et managed beans

--- src/main/java/managedBeans/DocumentManaged.java
+++ src/main/java/managedBeans/DocumentManaged.java
@@ -6,11 +6,15 @@
 package managedBeans;
 
 import beans.Document;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
+import tools.ConnectBDD;
 @ManagedBean(name="docM")
 @SessionScoped
 /**
@@ -32,13 +36,42 @@ public class DocumentManaged {
         this.selectedDocument = selectedDocument;
     }
     
-    public void getDocument() throws SQLException {
-        
+    public String getDocument() throws SQLException {
+        ConnectBDD b = new ConnectBDD();
+        if (b == null) {
+            throw new SQLException("Can't get database connection");
+        }
+        try {
+            /* Récupération des paramètres d'URL saisis par l'utilisateur */
+            String paramName = this.selectedDocument.getName();
+            /* Exécution d'une requête de modification de la BD (INSERT, UPDATE, DELETE, CREATE, etc.). */
+            b.getMyStatement().executeUpdate("INSERT INTO OBJECTS(ObjectsName) VALUES ('" + paramName + "');");
+            b.getMyConnexion().commit();
+            return "success";
+        } catch (SQLException ex) {
+            System.out.println("SQLException: " + ex.getMessage());
+            System.out.println("SQLState: " + ex.getSQLState());
+            System.out.println("VendorError: " + ex.getErrorCode());
+            return "failed";
+        }
     }
     
     public List<Document> putDocument() throws SQLException {
         List<Document> list = new ArrayList<>();
-        
+        //get database connection
+        ConnectBDD b = new ConnectBDD();
+        Connection con = b.getMyConnexion();
+        if (con == null) {
+            throw new SQLException("Can't get database connection");
+        }
+        PreparedStatement ps = con.prepareStatement("select * from OBJECTS;");
+        //get customer data from database
+        ResultSet result = ps.executeQuery();
+        while (result.next()) {
+            Document document = new Document();
+            document.setName(result.getString("ObjectsName"));
+            list.add(document);
+        }
         return list;
     }
 }
