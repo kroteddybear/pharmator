716b09e58c9f049345e887204adeff2539211855
jereem18 <jereem18@gmail.com>
2014-10-16 17:59:43 +0200

    ADDED: User,UserManaged login.xhtml

---
+++ src/main/java/beans/User.java
@@ -0,0 +1,56 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package beans;
+
+import javax.faces.bean.ManagedBean;
+
+/**
+ *
+ * @author jeremygillet
+ */
+
+@ManagedBean(name="User")
+public class User {
+
+    private String firstName;
+    private String lastName;
+    private String login;
+    private String password;
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String FirstName) {
+        this.firstName = FirstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String LastName) {
+        this.lastName = LastName;
+    }
+    
+}
+
